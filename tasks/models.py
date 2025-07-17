b21c156d2b412582d66932cdf397985210580695
AUSTIN GODFREY <17agodfrey@gmail.com>
2025-07-17 13:10:02 -0600

    commented out print statements in models.py

--- tasks/models.py
+++ tasks/models.py
@@ -40,18 +40,18 @@ class Task(models.Model):
     
 @receiver(post_save, sender=Task)
 def notify_due_soon(sender, instance, created, **kwargs):
-    print(f"Task {instance.title} created: {created}")
+    # print(f"Task {instance.title} created: {created}")
     if created:
         now_utc = timezone.now()
         due_utc = instance.due_date
         now = timezone.localtime(now_utc)
         due_date_local = timezone.localtime(due_utc)
         one_hour = now + timezone.timedelta(hours=1)
-        print(f"Current local time: {now}, One hour from now: {one_hour}, Task due date (local): {due_date_local}")
-        print(f"now tzinfo: {now.tzinfo}, due_date tzinfo: {due_date_local.tzinfo}")
+        # print(f"Current local time: {now}, One hour from now: {one_hour}, Task due date (local): {due_date_local}")
+        # print(f"now tzinfo: {now.tzinfo}, due_date tzinfo: {due_date_local.tzinfo}")
 
         if now <= due_date_local <= one_hour:
-            print(f"Task {instance.title} is due within the next hour, sending email notification.")
+            # print(f"Task {instance.title} is due within the next hour, sending email notification.")
             send_mail(
                 subject='Task due soon!',
                 message=f'Your task "{instance.title}" is due within the hour!',
